c45451db718d3fb256388d346988e5616a2705b6
jesus.real <8238188+jesusreal@users.noreply.github.com>
2019-01-20 22:28:50 +0100

    Add backup feature

---
+++ scripts/backup-restore.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# https://docs.docker.com/storage/volumes/#backup-restore-or-migrate-data-volumes
+
+# If $1 set to 'all' then take the latest complete backup
+BACKUP_TYPE=$1
+BACKUP_FILENAME=$2
+
+BACKUP_FILENAME_LATEST="backup-latest.tar"
+if [ -z $BACKUP_FILENAME ]; then
+  BACKUP_FILENAME=$BACKUP_FILENAME_LATEST
+fi
+
+MONGO_DB_CONTAINER=`docker ps -aqf name=learn-with-jesus_mongo_1`
+
+echo "Restoring from backup $BACKUP_DIR/$BACKUP_FILENAME"
+
+# DELETE EXISTENT DB AND COPY BACK  UP
+if [[ $BACKUP_TYPE == "all" ]]; then
+  BACKUP_DIR=$(pwd)/../data/backup-all
+  docker run --rm \
+    --volumes-from ${MONGO_DB_CONTAINER} \
+    -v $BACKUP_DIR:/backup \
+    ubuntu bash -c "cd data/db && rm -rf * && cd ../../ && tar xvf backup/${BACKUP_FILENAME}"
+else
+  BACKUP_DIR=$(pwd)/../data/backup
+  docker run --rm \
+    --volumes-from ${MONGO_DB_CONTAINER} \
+    -v $BACKUP_DIR:/backup \
+    ubuntu bash -c "cd data/db && rm -rf * && cd ../../ && tar xvf backup/${BACKUP_FILENAME} \
+    && cp -r /backup-tmp/** /data/db"
+fi
+
+
+
+
---
+++ scripts/backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# https://docs.docker.com/storage/volumes/#backup-restore-or-migrate-data-volumes
+
+# If set to 'all' then it will copy all files from data/db
+BACKUP_TYPE=$1
+
+TIMESTAMP=$(date +%s)
+MONGO_DB_CONTAINER=`docker ps -aqf name=learn-with-jesus_mongo_1`
+BACKUP_FILENAME=backup-${TIMESTAMP}.tar
+BACKUP_FILENAME_LATEST=backup-latest.tar
+
+echo "Creating backup $BACKUP_FILENAME $BACKUP_TYPE"
+
+if [[ $BACKUP_TYPE == "all" ]]; then
+  BACKUP_DIR=$(pwd)/../data/backup-all
+  docker run --rm \
+    --volumes-from ${MONGO_DB_CONTAINER} \
+    -v $BACKUP_DIR:/backup \
+    ubuntu tar cvf backup/${BACKUP_FILENAME} /data/db
+else
+  BACKUP_DIR=$(pwd)/../data/backup
+  docker run --rm \
+    --volumes-from ${MONGO_DB_CONTAINER} \
+    -v $BACKUP_DIR:/backup \
+    ubuntu bash -c "cp -r /data/db /backup-tmp && cd backup-tmp && rm -rf journal diagnostic.data local* && cd .. && tar cvf backup/${BACKUP_FILENAME} /backup-tmp"
+fi
+
+
+echo "Creating backup $BACKUP_FILENAME_LATEST"
+
+cd $BACKUP_DIR 
+cp $BACKUP_FILENAME $BACKUP_FILENAME_LATEST
